0d726678c9aad2e515deeb07bf39857e1e6fa48c
@ibrakhim-1825 <frontend1825@gmail.com>
2024-04-17 20:21:57 +0300

    qolgani

--- client/main.js
+++ client/main.js
@@ -1,15 +1,34 @@
 const box = document.querySelector(".box");
 
-
-
-
 const render = (users) => {
-    box.innerHTML = users.map((item) => `<h1>${item.name}</h1>
-    h1`)
+    box.innerHTML = users.map((i) => `<div class="card">
+    <h2>ID: ${i.id}</h2>
+    <h1>Name: ${i.name}</h1>
+    <h1>Username: ${i.username}</h1> 
+    <h1>Email: ${i.email}</h1>
+    <div class="addres">
+        <h3>Address:</h3>
+        <h3>Street: ${i.address.street}</h3>
+        <h3>Suite: ${i.address.suite}</h3>
+        <h3>City: ${i.address.city}</h3>
+        <h3>ZipCode: ${i.address.zipcode}</h3>
+        <h3>Geo:
+        <h3>lat: ${i.address.geo.lat}</h3>
+        <h3>lat: ${i.address.geo.lng}</h3>
+        </h3>
+        </div>
+        <h1>Phone: ${i.phone}</h1>
+        <h1>Web-site: ${i.website}</h1>
+        <h3>Company: ${i.company.name}</h3>
+        <h3>CatchPhrase: ${i.company.catchPhrase}</h3>
+        <h3>BS: ${i.company.bs}</h3>
+   
+    </div>`
+    ).join("")
 }
 
 const getDAta = () => {
-    fetch("http://localhost:3000/").then((res) => res.json()).then((data) => {
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json()).then((data) => {
         render(data);
     })
 };
